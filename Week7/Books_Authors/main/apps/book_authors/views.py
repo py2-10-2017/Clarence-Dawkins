b8788cc708870f8ac440b37db09fd08480b3e433
clarend <30541658+clarend@users.noreply.github.com>
2017-10-18 01:34:39 -0500

    Add files via upload

---
+++ Week7/Books_Authors/main/apps/book_authors/views.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return HttpResponse('Placeholder to later display all the list of books and authors')
